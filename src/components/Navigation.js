2d40f9a80f525fc4e22442af67d53602efb7dece
Marc Diaz <marcgabe15@gmail.com>
2019-09-13 08:40:56 -0400

    added new resume

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import "./navigation.css";
-import resume from '../images/marc_diaz_resume.pdf'
+import resume from '../images/Marc_Diaz_Resume.pdf'
 import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
